3faaf5a4d993ff89b19921797eb27bf09b684975
jake08bennett <jake08bennett@hotmail.com>
2015-09-20 09:41:12 -0600

    forgot to commit/push

--- methods.rb
+++ methods.rb
@@ -1,41 +1,36 @@
 module Methods
 	
+	def sleep_in?(weekday, vacation)
+		!weekday || vacation
+	end
+
+	def monkey_trouble?(a_smile, b_smile)
+		if a_smile && b_smile
+			return true
+		elsif !a_smile && !b_smile
+			return true
+		else
+			return false
+		end
+	end
+
+	def sum_double(a, b)
+		if a == b
+			(a + b)*2
+		else
+			a + b
+		end
+	end
+
+	def diff21(a)
+		n = (a - 21).abs
+		if a > 21
+			2 * n
+		else
+			n
+		end
+	end	
 	
-	# sleep_in?
-	#
-	# Students sleep in if it is not a weekday or we're on vacation. 
-	#
-	# ==== Attributes 
-	# 
-	# * weekday  - true only if it is a weekday
-	# * vacation - true only if the student is on vacation
-	#
-	# TODO - write sleep_in?
 
-	# monkey_trouble?
-	#
-	# We have two monkeys, a and b, and the parameters a_smile and b_smile 
-	# indicate if each is smiling. 
-	# We are in trouble if they are both smiling or if neither of them is smiling.
-	# Return true if we are in trouble.
-	#
-	# ==== Attributes 
-	# 
-	# * a_smile   - true only if monkey a is smiling
-	# * b_smile   - true only if monkey b is smiling
-	#
-	# TODO - write monkey_trouble?
-
-	# sum_double
-	#
-	# Given two int values, return their sum. Unless the two values are the same,
-	# then return double their sum. 
-	#
-	# ==== Attributes 
-	# 
-	# * a - an integer
-	# * b - an integer
-	#
-	# TODO - write sum_double
 
 end
